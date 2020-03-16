bbfe12d58a873d8d01aaa4218449f62588a794e2
tkyzmina <tkyzmina@gmail.com>
2020-03-16 21:24:14 +0300

    сорт по комментариям

--- js/sort.js
+++ js/sort.js
@@ -6,13 +6,15 @@
   var randomFilter = imgFilters.querySelector('#filter-random');
   var discussedFilter = imgFilters.querySelector('#filter-discussed');
 
+
   var buttonStyleApply = function (selectedFilter) {
     var activeFilter = imgFilters.querySelector('.img-filters__button--active');
     activeFilter.classList.remove('img-filters__button--active');
     selectedFilter.classList.add('img-filters__button--active');
   };
 
-  imgFilters.addEventListener('click', function (evt) {
+
+  var sortHandler = window.debounce(function (evt) {
     buttonStyleApply(evt.target);
     var choosenFilter = evt.target;
     switch (choosenFilter) {
@@ -24,20 +26,46 @@
         sortRandom();
         break;
       case discussedFilter:
+        sortPopular();
         console.log('discussedFilter');
         break;
     }
   });
 
+  imgFilters.addEventListener('click', sortHandler);
+
   var sortRandom = function () {
     var array = window.pictures.data;
 
     console.log(array);
-    // array.sort();
-    // console.log(array);
-    var yyy = array.slice(0, QUANTITY_RANDOM);
-    console.log(yyy);
-    window.pictures.insertPhoto(yyy);
+    var sortedArray = shuffle(array).slice(0, QUANTITY_RANDOM);
+    console.log(sortedArray);
+
+    window.pictures.insertPhoto(sortedArray);
+  };
+
+
+  var shuffle = function (array) {
+    var arrayCopy = array.slice();
+
+    for (var i = 0; i < arrayCopy.length; i++) {
+      var randomIndex = Math.floor(Math.random() * (i + 1));
+      var currentElement = arrayCopy[i];
+
+      arrayCopy[i] = arrayCopy[randomIndex];
+      arrayCopy[randomIndex] = currentElement;
+    }
+    return arrayCopy;
+  };
+
+
+  var sortPopular = function () {
+    var array = window.pictures.data;
+    array.sort(function (a, b) {
+      return b.comments.length - a.comments.length;
+    });
+    console.log(array);
+    window.pictures.insertPhoto(array);
   };
 
 })();
